5171abf97fc15312d4fd0103743ff22f8e7d7598
philemon <mmgen-py@yandex.com>
2013-12-17 12:20:28 +0400

    modified:   MANIFEST 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from distutils.core import setup
 
 setup(
 		name         = 'mmgen',
-		version      = '0.6.2',
+		version      = '0.6.3',
 		author       = 'Philemon',
 		author_email = 'mmgen-py@yandex.com',
 		url          = 'https://github.com/mmgen/mmgen',
 		py_modules = [
+			'mmgen.__init__',
 			'mmgen.addr',
 			'mmgen.bitcoin',
 			'mmgen.config',
 			'mmgen.license',
-			'mmgen.__init__',
 			'mmgen.mn_electrum',
 			'mmgen.mnemonic',
 			'mmgen.mn_tirosh',
@@ -20,8 +20,15 @@ setup(
 			'mmgen.tx',
 			'mmgen.utils',
 			'mmgen.walletgen',
-			'mmgen.connection',
-			'mmgen.proxy',
+
+			'mmgen.rpc.__init__',
+			'mmgen.rpc.config',
+			'mmgen.rpc.connection',
+			'mmgen.rpc.data',
+			'mmgen.rpc.exceptions',
+			'mmgen.rpc.proxy',
+			'mmgen.rpc.util',
+
 			'tests.addr',
 			'tests.bitcoin',
 			'tests.mn_electrum',
@@ -41,5 +48,9 @@ setup(
 			'mmgen-txcreate',
 			'mmgen-txsign',
 			'mmgen-txsend'
-		])]
+		])],
+		scripts=[
+			'scripts/bitcoind-walletunlock.py',
+			'scripts/deinstall.sh'
+		]
 	)
