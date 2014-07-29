6630f3c6ee70b4617b46ba4c62704739adcf1000
philemon <mmgen-py@yandex.com>
2014-07-30 00:59:34 +0400

    Version 0.7.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 		name         = 'mmgen',
-		version      = '0.7.6',
+		version     = '0.7.6',
 		author       = 'Philemon',
 		author_email = 'mmgen-py@yandex.com',
 		url          = 'https://github.com/mmgen/mmgen',
