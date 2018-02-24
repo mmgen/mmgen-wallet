4009e1794f4f8572a9a74e73f5349c57e68dcf7b
MMGen <mmgen@tuta.io>
2018-02-24 14:34:30 +0300

    test-release.sh: indentation fixes

--- scripts/test-release.sh
+++ scripts/test-release.sh
@@ -98,10 +98,10 @@ do_test() {
 i_obj='Data object'
 s_obj='Testing data objects'
 t_obj=(
-    'test/objtest.py --coin=btc -S'
-    'test/objtest.py --coin=btc --testnet=1 -S'
-    'test/objtest.py --coin=ltc -S'
-    'test/objtest.py --coin=ltc --testnet=1 -S')
+	'test/objtest.py --coin=btc -S'
+	'test/objtest.py --coin=btc --testnet=1 -S'
+	'test/objtest.py --coin=ltc -S'
+	'test/objtest.py --coin=ltc --testnet=1 -S')
 f_obj='Data object test complete'
 
 i_alts='Gen-only altcoin'
@@ -175,29 +175,29 @@ f_monero='Monero tests completed'
 i_misc_ni='Miscellaneous operations (non-interactive)'
 s_misc_ni='Testing miscellaneous operations (non-interactive)'
 t_misc_ni=(
-    'test/sha256test.py')
+	'test/sha256test.py')
 f_misc_ni='Miscellaneous non-interactive tests complete'
 
 i_misc='Miscellaneous operations (interactive)' # includes autosign!
 s_misc='The bitcoin, bitcoin-abc and litecoin (mainnet) daemons must be running for the following tests'
 t_misc=(
-    'test/test.py -On misc')
+	'test/test.py -On misc')
 f_misc='Miscellaneous interactive tests test complete'
 
 i_btc='Bitcoin mainnet'
 s_btc='The bitcoin (mainnet) daemon must both be running for the following tests'
 t_btc=(
-    'test/test.py -On'
+	'test/test.py -On'
 	'test/test.py -On --segwit dfl_wallet main ref ref_other'
 	'test/test.py -On --segwit-random dfl_wallet main'
-    'test/tooltest.py rpc'
+	'test/tooltest.py rpc'
 	"scripts/compute-file-chksum.py $REFDIR/*testnet.rawtx >/dev/null 2>&1")
 f_btc='You may stop the bitcoin (mainnet) daemon if you wish'
 
 i_btc_tn='Bitcoin testnet'
 s_btc_tn='The bitcoin testnet daemon must both be running for the following tests'
 t_btc_tn=(
-    'test/test.py -On --testnet=1'
+	'test/test.py -On --testnet=1'
 	'test/test.py -On --testnet=1 --segwit dfl_wallet main ref ref_other'
 	'test/test.py -On --testnet=1 --segwit-random dfl_wallet main'
 	'test/tooltest.py --testnet=1 rpc')
@@ -234,17 +234,17 @@ f_b2x_rt='Regtest (Bob and Alice) mode tests for B2X completed'
 i_ltc='Litecoin'
 s_ltc='The litecoin daemon must both be running for the following tests'
 t_ltc=(
-    'test/test.py --coin=ltc -On dfl_wallet main'
-    'test/test.py --coin=ltc --segwit -On dfl_wallet main'
-    'test/test.py --coin=ltc --segwit-random -On dfl_wallet main'
-    'test/tooltest.py --coin=ltc rpc'
+	'test/test.py --coin=ltc -On dfl_wallet main'
+	'test/test.py --coin=ltc --segwit -On dfl_wallet main'
+	'test/test.py --coin=ltc --segwit-random -On dfl_wallet main'
+	'test/tooltest.py --coin=ltc rpc'
 )
 f_ltc='You may stop the litecoin daemon if you wish'
 
 i_ltc_tn='Litecoin testnet'
 s_ltc_tn='The litecoin testnet daemon must both be running for the following tests'
 t_ltc_tn=(
-    'test/test.py --coin=ltc -On --testnet=1'
+	'test/test.py --coin=ltc -On --testnet=1'
 	'test/test.py --coin=ltc -On --testnet=1 --segwit dfl_wallet main ref ref_other'
 	'test/test.py --coin=ltc -On --testnet=1 --segwit-random dfl_wallet main'
 	'test/tooltest.py --coin=ltc --testnet=1 rpc')
@@ -281,13 +281,13 @@ t_gen=(
 	"test/gentest.py -q 2 $REFDIR/btcwallet.dump"
 	'test/gentest.py -q 1:2 10'
 	'test/gentest.py -q --type=segwit 1:2 10'
-    "test/gentest.py -q --testnet=1 2 $REFDIR/btcwallet-testnet.dump"
+	"test/gentest.py -q --testnet=1 2 $REFDIR/btcwallet-testnet.dump"
 	'test/gentest.py -q --testnet=1 1:2 10'
 	'test/gentest.py -q --testnet=1 --type=segwit 1:2 10'
-    "test/gentest.py -q --coin=ltc 2 $REFDIR/litecoin/ltcwallet.dump"
+	"test/gentest.py -q --coin=ltc 2 $REFDIR/litecoin/ltcwallet.dump"
 	'test/gentest.py -q --coin=ltc 1:2 10'
 	'test/gentest.py -q --coin=ltc --type=segwit 1:2 10'
-    "test/gentest.py -q --coin=ltc --testnet=1 2 $REFDIR/litecoin/ltcwallet-testnet.dump"
+	"test/gentest.py -q --coin=ltc --testnet=1 2 $REFDIR/litecoin/ltcwallet-testnet.dump"
 	'test/gentest.py -q --coin=ltc --testnet=1 1:2 10'
 	'test/gentest.py -q --coin=ltc --testnet=1 --type=segwit 1:2 10'
 	)
@@ -301,18 +301,18 @@ f_gen='gentest tests completed'
 [ "$INSTALL_ONLY" ] && exit
 
 skip_maybe() {
-    echo -n "Enter 's' to skip, or ENTER to continue: "; read
-    [ "$REPLY" == 's' ] && return 0
-    return 1
+	echo -n "Enter 's' to skip, or ENTER to continue: "; read
+	[ "$REPLY" == 's' ] && return 0
+	return 1
 }
 
 run_tests() {
 	for t in $1; do
-        eval echo -e \${GREEN}'###' Running $(echo \$i_$t) tests\$RESET
-        [ "$PAUSE" ] && { eval echo $(echo \$s_$t); skip_maybe && continue; }
-#       echo RUNNING
+		eval echo -e \${GREEN}'###' Running $(echo \$i_$t) tests\$RESET
+		[ "$PAUSE" ] && { eval echo $(echo \$s_$t); skip_maybe && continue; }
+#		echo RUNNING
 		eval "do_test \"\${t_$t[@]}\""
-        eval echo -e \$GREEN$(echo \$f_$t)\$RESET
+		eval echo -e \$GREEN$(echo \$f_$t)\$RESET
 	done
 }
 
