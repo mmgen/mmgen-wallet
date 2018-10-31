b558d7bcb0fece68df9fef8c18c99d05fb1c9f82
MMGen <mmgen@tuta.io>
2018-10-31 19:12:29 +0000

    py3port complete
    When the Python 3 port has received sufficient testing, it will be merged into
    the master branch.  From then on, MMGen will no longer support Python 2.
    
    Users who wish to run current MMGen versions should upgrade their systems with
    Python 3 versions of the prerequisite libraries listed on the
    Install-MMGen-on-Debian-or-Ubuntu-Linux wiki page.

--- mmgen/globalvars.py
+++ mmgen/globalvars.py
@@ -38,8 +38,8 @@ class g(object):
 
 	# Constants:
 
-	version      = '0.9.999'
-	release_date = 'October 2018'
+	version      = '0.9.999py3'
+	release_date = 'November 2018'
 
 	proj_name = 'MMGen'
 	proj_url  = 'https://github.com/mmgen/mmgen'
