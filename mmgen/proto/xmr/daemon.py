b606b47fcc676c98b5ee9013ff682b80b35f0983
The MMGen Project <mmgen@tuta.io>
2023-04-18 18:35:57 +0000

    update Monero daemon support to v0.18.2.2

--- mmgen/proto/xmr/daemon.py
+++ mmgen/proto/xmr/daemon.py
@@ -19,7 +19,7 @@ from ...util import list_gen,die,contains_any
 from ...daemon import CoinDaemon,RPCDaemon,_nw,_dd
 
 class monero_daemon(CoinDaemon):
-	daemon_data = _dd('Monero', 18002000, '0.18.2.0-release')
+	daemon_data = _dd('Monero', 18002002, '0.18.2.2-release')
 	networks = ('mainnet','testnet')
 	exec_fn = 'monerod'
 	testnet_dir = 'stagenet'
