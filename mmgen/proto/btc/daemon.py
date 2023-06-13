903b4265a5752eb1058dd186393a09d766e9e0e5
The MMGen Project <mmgen@tuta.io>
2023-06-13 18:32:21 +0000

    support Bitcoin Core v25.0.0

--- mmgen/proto/btc/daemon.py
+++ mmgen/proto/btc/daemon.py
@@ -19,7 +19,7 @@ from ...util import list_gen
 from ...daemon import CoinDaemon,_nw,_dd
 
 class bitcoin_core_daemon(CoinDaemon):
-	daemon_data = _dd('Bitcoin Core', 240001, '24.0.1')
+	daemon_data = _dd('Bitcoin Core', 250000, '25.0.0')
 	exec_fn = 'bitcoind'
 	cli_fn = 'bitcoin-cli'
 	testnet_dir = 'testnet3'
