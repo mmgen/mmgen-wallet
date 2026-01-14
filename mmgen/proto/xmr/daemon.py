d3095b159e5604614ca8e007b89493cd42dd9080
The MMGen Project <mmgen@tuta.io>
2026-01-14 16:27:33 +0000

    support Monero v0.18.4.5

--- mmgen/proto/xmr/daemon.py
+++ mmgen/proto/xmr/daemon.py
@@ -19,7 +19,7 @@ from ...util import list_gen, die, contains_any
 from ...daemon import CoinDaemon, RPCDaemon, _nw, _dd
 
 class monero_daemon(CoinDaemon):
-	daemon_data = _dd('Monero', 18004003, '0.18.4.3-release')
+	daemon_data = _dd('Monero', 18004005, '0.18.4.5-release')
 	networks = ('mainnet', 'testnet')
 	exec_fn = 'monerod'
 	testnet_dir = 'stagenet'
