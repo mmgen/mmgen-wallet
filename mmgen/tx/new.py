mmgen/mmgen-wallet

commit 907bdc2bdf152efc6e6be694cd83657ae033c38f

XMR compat: sweep transactions
Example:

    # Invoking `mmgen-txcreate` without arguments activates the sweep function-
    # ality.  Type ‘S’ (account sweep) or ‘s’ (address sweep) to begin creating
    # the transaction:
    $ mmgen-txcreate --coin=xmr

Testing/demo:

    $ test/cmdtest.py --coin=xmr -e -X alice_twview_chk4 xmr_compat

mmgen/tx/new.py
@@ -79,6 +79,7 @@ def parse_fee_spec(proto, fee_arg):
 class New(Base):
 
 	fee_is_approximate = False
+	is_sweep = False
 	msg_wallet_low_coin = 'Wallet has insufficient funds for this transaction ({} {} needed)'
 	msg_no_change_output = """
 		ERROR: No change address specified.  If you wish to create a transaction with
@@ -455,7 +456,7 @@ class New(Base):
 		if self.cfg.comment_file:
 			self.add_comment(infile=self.cfg.comment_file)
 
-		if not do_info:
+		if cmd_args and not do_info:
 			cmd_args, addrfile_args = self.get_addrfiles_from_cmdline(cmd_args)
 			if self.is_swap:
 				cmd_args = await self.process_swap_cmdline_args(cmd_args, addrfile_args)
@@ -475,7 +476,8 @@ class New(Base):
 				self.cfg,
 				self.proto,
 				minconf = self.cfg.minconf,
-				addrs = await self.get_input_addrs_from_inputs_opt())
+				addrs = await self.get_input_addrs_from_inputs_opt(),
+				tx = self if self.is_sweep else None)
 			await self.twuo.get_data()
 			self.twctl = self.twuo.twctl
 
@@ -484,6 +486,11 @@ class New(Base):
 
 		if not (self.is_bump or self.cfg.inputs):
 			await self.twuo.view_filter_and_sort()
+
+		if self.is_sweep:
+			del self.twctl
+			del self.twuo.twctl
+			return await self.compat_create()
 
 		if not self.is_bump:
 			self.twuo.display_total()

mmgen/proto/xmr/tx/new.py
@@ -18,6 +18,11 @@ from .base import Base
 
 class New(Base, TxNew):
 
+	async def create(self, cmd_args, **kwargs):
+		self.is_sweep = not cmd_args
+		self.sweep_spec = None
+		return await super().create(cmd_args, **kwargs)
+
 	async def get_input_addrs_from_inputs_opt(self):
 		return [] # TODO
 
@@ -42,7 +47,20 @@ class New(Base, TxNew):
 
 	async def compat_create(self):
 
-		if True:
+		if self.is_sweep:
+			if not self.sweep_spec:
+				from ....util import ymsg
+				ymsg('No transaction operation specified. Exiting')
+				return None
+			from ....ui import item_chooser
+			from ....color import pink
+			op = item_chooser(
+				self.cfg,
+				'Choose the sweep operation type',
+				('sweep', 'sweep_all'),
+				lambda s: pink(s.upper())).item
+			spec = self.sweep_spec
+		else:
 			op = 'transfer'
 			i = self.inputs[0]
 			o = self.outputs[0]

mmgen/data/version
@@ -1,1 +1,1 @@
-16.1.dev25
+16.1.dev26

mmgen/help/help_notes.py
@@ -23,7 +23,7 @@ class help_notes:
 			case 'Bitcoin':
 				return '[ADDR,AMT ... | DATA_SPEC] ADDR [addr file ...]'
 			case 'Monero':
-				return 'ADDR,AMT'
+				return '[ADDR,AMT]'
 			case _:
 				return 'ADDR,AMT [addr file ...]'
 

mmgen/help/txcreate_examples.py
@@ -22,6 +22,7 @@ def help(proto, cfg):
 	addr = t.privhex2addr('bead' * 16)
 	sample_addr = addr.views[addr.view_pref]
 	cmd_base = gc.prog_name + ('' if proto.coin == 'BTC' else f' --coin={proto.coin.lower()}')
+	action = 'Create' if gc.prog_name == 'mmgen-txcreate' else 'Execute'
 
 	match proto.base_proto:
 		case 'Bitcoin':
@@ -64,6 +65,10 @@ EXAMPLES:
   Send 0.123 {proto.coin} to an external {proto.name} address:
 
     $ {cmd_base} {sample_addr},0.123
+
+  {action} a sweep transaction:
+
+    $ {cmd_base}
 """
 
 		case _:

mmgen/main_txcreate.py
@@ -130,7 +130,7 @@ if cfg.list_assets:
 	Msg('AVAILABLE SWAP ASSETS:\n' + sp.SwapAsset('BTC', 'send').fmt_assets_data(indent='  '))
 	sys.exit(0)
 
-if not (cfg.info or cfg.contract_data):
+if not (cfg.info or cfg.contract_data or cfg.coin == 'XMR'):
 	if len(cfg._args) < {'tx': 1, 'swaptx': 2}[target]:
 		cfg._usage()
 

mmgen/proto/xmr/tw/unspent.py
@@ -30,4 +30,40 @@ class MoneroTwUnspentOutputs(MoneroTwView, TwUnspentOutputs):
 		self.extra_key_mappings = {
 			'R': 'a_sync_wallets',
 			'A': 's_age'}
+		if tx and tx.is_sweep:
+			self.prompt_fs_in[-1] = 'Actions: [q]uit, add [l]abel, r[e]draw, [R]efresh balances:'
+			self.prompt_fs_in.insert(-1, 'Transaction ops: [s]weep to address, [S]weep to account')
+			self.extra_key_mappings.update({
+				's': 'i_addr_sweep',
+				'S': 'i_acct_sweep'})
 		await super().__init__(cfg, proto, minconf=minconf, addrs=addrs, tx=tx)
+
+	async def get_idx_from_user(self, method_name):
+		if method_name in ('i_acct_sweep', 'i_addr_sweep'):
+			from collections import namedtuple
+			ret = []
+			for acct_desc in {
+					'i_acct_sweep': ['source', 'destination'],
+					'i_addr_sweep': ['source']}[method_name]:
+				if res := await self.get_idx(f'{acct_desc} account number', self.accts_data):
+					ret.append(res)
+				else:
+					return None
+			return namedtuple('usr_idx_data', 'idx acct_addr_idx', defaults=[None])(*ret)
+		else:
+			return await super().get_idx_from_user(method_name)
+
+	class item_action(TwUnspentOutputs.item_action):
+		acct_methods = ('i_acct_sweep', 'i_addr_sweep')
+
+		async def i_acct_sweep(self, parent, idx, acct_addr_idx=None):
+			d = parent.accts_data
+			d1 = d[idx.idx - 1]
+			d2 = d[acct_addr_idx.idx - 1]
+			parent.tx.sweep_spec = f'{d1.idx}:{d1.acct_idx},{d2.idx}:{d2.acct_idx}'
+			return 'quit_view'
+
+		async def i_addr_sweep(self, parent, idx, acct_addr_idx=None):
+			d = parent.accts_data[idx.idx - 1]
+			parent.tx.sweep_spec = f'{d.idx}:{d.acct_idx}'
+			return 'quit_view'

mmgen/tw/unspent.py
@@ -95,7 +95,8 @@ class TwUnspentOutputs(TwView):
 			self.__dict__['proto'] = proto
 			MMGenListItem.__init__(self, **kwargs)
 
-	async def __init__(self, cfg, proto, *, minconf=1, addrs=[]):
+	async def __init__(self, cfg, proto, *, minconf=1, addrs=[], tx=None):
+		self.tx  = tx
 		await super().__init__(cfg, proto)
 		self.minconf  = NonNegativeInt(minconf)
 		self.addrs    = addrs

commit 74acc44a2c083bd2e74160a8071abb5f3fd48824

tx.new: fix ETH contract creation regression (bugfix)
Regression was introduced in commit 907bdc2bd

mmgen/tx/new.py
@@ -456,7 +456,7 @@ class New(Base):
 		if self.cfg.comment_file:
 			self.add_comment(infile=self.cfg.comment_file)
 
-		if cmd_args and not do_info:
+		if not (do_info or self.is_sweep):
 			cmd_args, addrfile_args = self.get_addrfiles_from_cmdline(cmd_args)
 			if self.is_swap:
 				cmd_args = await self.process_swap_cmdline_args(cmd_args, addrfile_args)