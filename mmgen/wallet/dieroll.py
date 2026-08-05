39c9086472365276a7af41501369ccc9c2bd3ee5
The MMGen Project <mmgen@tuta.io>
2026-08-05 18:32:05 +0000

    wallet.dieroll: increase OS random bytes to 32

--- mmgen/wallet/dieroll.py
+++ mmgen/wallet/dieroll.py
@@ -59,6 +59,7 @@ class wallet(wallet):
 				from ..crypto import Crypto
 				seed_bytes = Crypto(self.cfg).add_user_random(
 					rand_bytes = seed_bytes,
+					osrand_bytes = 32,
 					desc = 'gathered from your die rolls')
 				self.desc += ' plus OS and user-supplied entropy'
 			msg('')
